b1662b332ea8ab09314b9af96fb03a2af3d52d34
Artyom <cash.kin.art@gmail.com>
2023-10-25 18:43:32 +0300

    сÑthe code works )))))

--- components/GlobalWrapper/GlobalWrapper.jsx
+++ components/GlobalWrapper/GlobalWrapper.jsx
@@ -1,7 +1,39 @@
 import Header from '../Header';
 import styles from './globalWrapper.module.scss';
+import Image from "next/image";
+import { useEffect, useRef } from 'react';
 
 const GlobalWrapper = ({ children }) => {
+  const stickyRef = useRef(null);
+  const scrollSectionRef = useRef(null);
+
+  const images = [
+    '/car.jpg',
+    '/car2.jpeg',
+    '/forest.jpg',
+    '/house.jpg'
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      transform();
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  function transform() {
+    const offsetTop = stickyRef.current.parentElement.offsetTop;
+    const scrollSection = scrollSectionRef.current;
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+    percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
+    scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
+  }
+
   return (
     <>
       <Header />
@@ -18,8 +50,20 @@ const GlobalWrapper = ({ children }) => {
       </section>
 
       <div className={styles.sticky_parent}>
-        <div className={styles.sticky}>
-          <div className={styles.scroll_section} />
+        <div ref={stickyRef} className={styles.sticky}>
+          <div ref={scrollSectionRef} className={styles.scroll_section}>
+            {images.map((imgSrc, index) => (
+              <Image
+                key={index}
+                src={imgSrc}
+                alt="Image Alt Text"
+                className={styles.img}
+                width={1024}
+                height={578}
+                priority
+              />
+            ))}
+          </div>
         </div>
       </div>
 
@@ -35,12 +79,6 @@ const GlobalWrapper = ({ children }) => {
         </div>
       </section>
 
-      <div className={styles.sticky_parent}>
-        <div className={styles.sticky}>
-          <div className={styles.scroll_section} />
-        </div>
-      </div>
-
       <section className={styles.section}>
         <div className={styles.container}>
           <h1>End</h1>
@@ -52,6 +90,7 @@ const GlobalWrapper = ({ children }) => {
             facilis soluta, ut.</p>
         </div>
       </section>
+
       {children}
     </>
   );
